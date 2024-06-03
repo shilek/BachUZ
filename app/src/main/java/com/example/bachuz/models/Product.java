3b88021fccf7c78b2a0b8c23fd3231f7288597a1
shilek <dawids8.20098@gmail.com>
2024-06-03 18:14:39 +0200

    added shopping cart handling (don't read code for your health safety XDDDD)

---
+++ app/src/main/java/com/example/bachuz/models/Product.java
@@ -0,0 +1,11 @@
+package com.example.bachuz.models;
+
+public class Product {
+    public String productName;
+    public int productCount;
+
+    public Product(String name, int count) {
+        productName = name;
+        productCount = count;
+    }
+}
